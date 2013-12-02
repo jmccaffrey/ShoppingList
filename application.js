b5d1d5a6155e22b5a084dac59618c49f8358eddc
Davo <pog@pogi.net>
2013-12-02 08:27:39 -0800

    regex fix to allow spaces, min-width fix, line-height hack

--- application.js
+++ application.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
             return false;
         else if (String(entry).length > 20)
             return String(entry).substring(0,20);
-        else if (String(entry).match(/\s+/))
+        else if (String(entry).match(/^[\s+]$/))
             return false;
         else
             return String(entry).trim();
